d62b9598f64aefa779f9be72df0eb9ee72e1888e
MIDPEBA <31540561+MIDPEBA@users.noreply.github.com>
2017-10-03 20:36:53 +0200

    c1

---
+++ status/run.ps1
@@ -0,0 +1,12 @@
+# POST method: $req
+$requestBody = Get-Content $req -Raw | ConvertFrom-Json
+$name = $requestBody.name
+
+# GET method: each querystring parameter is its own variable
+if ($req_query_name) 
+{
+    $name = $req_query_name 
+}
+
+Out-File -Encoding Ascii -FilePath $res -inputObject "Hello1 $name"
+#GFET
