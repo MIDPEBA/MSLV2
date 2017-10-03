46b2385af74226a06289489fba0d1103aecc00d0
MIDPEBA <31540561+MIDPEBA@users.noreply.github.com>
2017-10-03 22:55:18 +0200

    c5

--- broker/run.ps1
+++ broker/run.ps1
@@ -1,11 +1,2 @@
-# POST method: $req
-$requestBody = Get-Content $req -Raw | ConvertFrom-Json
-$name = $requestBody.name
-
-# GET method: each querystring parameter is its own variable
-if ($req_query_name) 
-{
-    $name = $req_query_name 
-}
-
-Out-File -Encoding Ascii -FilePath $res -inputObject "Hello $name"
+Out-File -Encoding Ascii -FilePath $res -inputObject "OK!"
+Get-Content $req -Raw | Out-File $outputQueueItem
