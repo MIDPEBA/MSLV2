fd9529c749367bffb66da9401324a6a0f13c9fe2
MIDPEBA <31540561+MIDPEBA@users.noreply.github.com>
2017-10-04 23:37:26 +0200

    wokerkoden

--- worker/run.ps1
+++ worker/run.ps1
@@ -1,2 +1,17 @@
-$in = Get-Content $triggerInput -Raw
-Write-Output "PowerShell script processed queue message '$in'"
+$in = Get-Content $triggerInput -Raw | ConvertFrom-Json
+
+#Write-Output "PowerShell script processed queue message '$in'"
+
+Connect-PnPOnline -Url "https://onmidroc.sharepoint.com/teams/SC000898" -AppId 79a6a8d6-d94e-4c87-bce4-9ccaff8082fe -AppSecret $env:AppSecret
+$myList = Get-PnPList -Identity myList
+Write-Output $myList.title
+Add-PnPListItem -List myList -Values @{"Title" = $in.name} -ContentType "Item"
+Disconnect-PnPOnline
+
+$entity = [PSObject]@{
+  PartitionKey = "test"  
+  RowKey = get-date -Format "yyyy-MM-dd H:m:s.ms"
+  Name= $in.name
+}
+
+$entity | ConvertTo-Json | Out-File -Encoding UTF8 $outputTable
